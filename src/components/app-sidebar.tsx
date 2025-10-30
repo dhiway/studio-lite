8aa515445589fce80ce617e897ba540346799fb3
Sujith Barve <sujith.baruve@dhiway.com>
2025-10-30 21:47:29 +0530

    sidebar added
    Signed-off-by: Sujith Barve <sujith.baruve@dhiway.com>

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,104 @@
+import * as React from "react"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: "Choose Schema",
+      url: "#",
+      items: [],
+    },
+    {
+      title: "Recently Used Schema",
+      url: "#",
+      items: [
+         {
+          title: "ID Card",
+          url: "#",
+        },
+         {
+          title: "Grade Sheet",
+          url: "#",
+        },
+          {
+          title: "Course Certificate",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "All Issued Credentials",
+      url: "#",
+      items: [],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                {/* <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <GalleryVerticalEnd className="size-4" />
+                </div> */}
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-medium">MARK Lite</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {data.navMain.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url} className="font-medium">
+                    {item.title}
+                  </a>
+                </SidebarMenuButton>
+                {item.items?.length ? (
+                  <SidebarMenuSub>
+                    {item.items.map((item) => (
+                      <SidebarMenuSubItem key={item.title}>
+                        <SidebarMenuSubButton asChild isActive={item.isActive}>
+                          <a href={item.url}>{item.title}</a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                ) : null}
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+       <SidebarFooter>
+        <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-medium">Organisation Settings</span>
+                </div>
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
