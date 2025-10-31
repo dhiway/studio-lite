af4ad571d50de0a826accd5b24cb76d09270cd13
Sujith Barve <sujith.baruve@dhiway.com>
2025-10-31 23:36:53 +0530

    make poppins default and other ui changes
    Signed-off-by: Sujith Barve <sujith.baruve@dhiway.com>

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,17 +1,7 @@
 import { AppSidebar } from "@/components/app-sidebar"
-// import {
-//   Breadcrumb,
-//   BreadcrumbItem,
-//   BreadcrumbLink,
-//   BreadcrumbList,
-//   BreadcrumbPage,
-//   BreadcrumbSeparator,
-// } from "@/components/ui/breadcrumb"
-// import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
   SidebarProvider,
-  // SidebarTrigger,
 } from "@/components/ui/sidebar"
 
 export default function Page() {
@@ -19,19 +9,8 @@ export default function Page() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        {/* <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-          <div className="flex items-center gap-2 px-3">
-            <SidebarTrigger />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-          </div>
-        </header> */}
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+        <div className="flex flex-1 flex-col gap-0 p-0">
+          <div className="min-h-screen bg-[#212121] [background-image:linear-gradient(to_right,#393939_1px,transparent_1px),linear-gradient(to_bottom,#2a2a2a_1px,transparent_1px)] bg-[size:40px_40px]"></div>
         </div>
       </SidebarInset>
     </SidebarProvider>
