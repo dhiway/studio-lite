b5ee496d6b0e881a8e67a2d2727e814e3e9be229
Sujith Barve <sujith.baruve@dhiway.com>
2026-01-21 22:47:37 +0530

    feat: Schema based form
    Signed-off-by: Sujith Barve <sujith.baruve@dhiway.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import path from "path"
+import { defineConfig } from "vite"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
+import react from "@vitejs/plugin-react-swc"
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(), tailwindcss(),tanstackRouter({
-      target: 'react',
+  plugins: [
+    tanstackRouter({
+      target: "react",
       autoCodeSplitting: true,
-    }),],
+    }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
